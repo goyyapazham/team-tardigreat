18cd36bae90265a0c3a4126763ea1c1adfb57234
st234pa <syoon1@stuy.edu>
2016-11-22 15:08:20 -0500

    started turtleshell

---
+++ turtleshell.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void parse() {
+}
+
+void execute(){
+}
+
+int main() {
+  char command[100];
+  int i;
+  char* word[50];
+  while(true) {
+    parse();
+    execute();
+  }
+  return 0;
+}
