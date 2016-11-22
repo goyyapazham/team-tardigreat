6256867404e1f75d09733887d344a742a3a14e6b
st234pa <syoon1@stuy.edu>
2016-11-22 15:19:16 -0500

    :/

--- turtleshell.c
+++ turtleshell.c
@@ -3,19 +3,27 @@
 #include <unistd.h>
 #include <string.h>
 
-void parse() {
+void parse(char* command, char** word) {
+  *strchr(command, '\n') = 0;
+  int i = 0;
+  char *x = command;
+  while(word[i] = strsep(&x, " ")) i++;
 }
 
-void execute(){
+void execute(char** word){
+  int f = fork();
+  execvp(word[0], word);
 }
 
 int main() {
   char command[100];
   int i;
   char* word[50];
-  while(true) {
-    parse();
-    execute();
+  while(1) {
+    printf("Turtleshell: ");
+    fgets(command, sizeof(command), stdin);
+    parse(command, word);
+    execute(word);
   }
   return 0;
 }
