3d15ed4da49e0ce89253d076cf82446f60526344
goyyapazham <nalanda99@gmail.com>
2016-11-26 14:23:27 -0500

    alskdj

--- turtleshell.c
+++ turtleshell.c
@@ -6,13 +6,6 @@
 #include "parse.h"
 #include "turtleshell.h"
 
-void parse(char* command, char** word) {
-  *strchr(command, '\n') = 0;
-  int i = 0;
-  char *x = command;
-  while(word[i] = strsep(&x, " ")) i++;
-}
-
 void execute(char** word){
   int f = fork();
   if (f == -1) {
